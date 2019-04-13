82dcc6612d58e0fc7070cd2e92f94207e454ca10
38tter <odradek38@gmail.com>
2019-04-14 01:16:32 +0900

    Add some comments.

--- src/chartest.py
+++ src/chartest.py
@@ -2,7 +2,7 @@ from gensim.models.word2vec import Word2Vec
 import gensim
 import sys
 
-JPmodel = Word2Vec.load('mychar2vec_fromWikiALL.model')
+JPmodel = Word2Vec.load('mychar2vec_fromWikiALL.model') # Learned from Japanese character
 
 out = JPmodel.most_similar(positive=["人"], negative=["王"])
 
